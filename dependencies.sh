792c3967dd86a506d17c9b8615c476d6da72fd0c
Alex <akleider@sonic.net>
2017-11-16 10:15:05 -0800

    Add DEBIAN_FRONTEND=noninteractive to dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 # File: dependencies.sh
+
+# Purpose is to install the pathagar dependencies.
+
 # It is assumed that your OS has python 2.7 as the default python.
 
 set -o errexit  # ends if an error is returned.
 set -o pipefail # pipe failure causes an error.
 set -o nounset  # ends if an undefined variable is encountered.
+set -x          # all commands are echoed to the terminal
 
-apt-get -y install apache2
-apt-get -y install python-pip python-virtualenv
-apt-get -y install python-dev libapache2-mod-wsgi
-apt-get -y install libmysqlclient-dev libxml2-dev libxslt1-dev
-apt-get -y install mysql-server  # the default is sqlite3
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install \
+  apache2 \
+  python-pip \
+  python-virtualenv \
+  python-dev \
+  libapache2-mod-wsgi \
+  libmysqlclient-dev \
+  libxml2-dev \
+  libxslt1-dev \
+  mysql-server
 
-shutdown -r now
+# May need to reboot here??
+# shutdown -r now
+
+
+
