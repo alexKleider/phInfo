e7a84beaf699d9e409c172593d66faa0c3058cd8
Alex <akleider@sonic.net>
2017-11-23 22:13:06 -0800

    Fix if statement in repo.sh

--- repo.sh
+++ repo.sh
@@ -3,7 +3,7 @@
 # File: repo.sh
 
 echo "Installing git..."
-if [ sudo apt-get -y install git ]
+if  sudo apt-get -y install git 
 then
     echo "... git successfully installed."
 else
@@ -15,7 +15,7 @@ echo "Changing into the users home directory."
 cd ${HOME}
 
 echo "Cloning phInfo repository..."
-if [ git clone https://github.com/alexKleider/phInfo.git ]
+if  git clone https://github.com/alexKleider/phInfo.git 
 then
     echo "... phInfo repository successfully cloned."
     echo "You'll have to 'cd phInfo', script can't do it."
