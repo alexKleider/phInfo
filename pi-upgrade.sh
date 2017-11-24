284b94198c9dde5835a60c5fb0bafb563a301090
Alex <akleider@sonic.net>
2017-11-23 18:23:06 -0800

    Comment pi-upgrade.sh to see if it runs

--- pi-upgrade.sh
+++ pi-upgrade.sh
@@ -5,10 +5,13 @@
 set -o errexit  # ends if an error is returned.
 set -x          # Echo commands to stdout.
 
+echo "Running edit-scripts version of pi-upgrade.sh"
+
 sudo apt --fix-missing update
 sudo apt -y --fix-missing upgrade
 sudo apt -y dist-upgrade
 
+echo "Finished running edit-scripts version of pi-upgrade.sh"
 sudo shutdown -r now
 # a reboot is required for the latest kernel to be implemented.
 
